a942e8d06a7576bf30ba06e90ee7f0253dd29b1d
Marco Favorito <marco.favorito@gmail.com>
2017-04-05 18:46:57 +0200

    algorithm implementation started

--- src/hw.sh
+++ src/hw.sh
@@ -3,15 +3,19 @@ mkdir output
 ./create_collection.sh 
 
 
-stemmers=("default stemmer", "EnglishStemmer​", "EnglishStemmer​StopWords")
+stemmers=("default_stemmer" "EnglishStemmer​" "EnglishStemmer​StopWords")
 scorers=("CountScorer" "TfIdfScorer" "BM25Scorer")
+modes=("text_and_title" "text" "title")
 for index in ${!stemmers[*]}; do
 	current_stemmer=${stemmers[$index]}
 	echo "Using stemmer ${current_stemmer}"
 	./create_index.sh cran.collection "$index"
 	for current_scorer in ${scorers[*]}; do
 		echo "Run all queries using the scorer: ${current_scorer}"
-		java homework.RunAllQueries_HW "cran" cran_all_queries.tsv "${current_scorer}" "text_and_title" "output/output_cran__${current_stemmer}__${current_scorer}.tsv"
+		for current_mode in ${modes[*]}; do
+			echo "Using mode ${current_mode}"
+			java homework.RunAllQueries_HW "cran" cran_all_queries.tsv "${current_scorer}" "${current_mode}" "output/output_cran__${current_stemmer}__${current_scorer}__${current_mode}.tsv"
+		done
 	done
 	rm cran-*
 done
